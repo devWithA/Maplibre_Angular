2ff909923db8f4d1810df51745ac4fc7a71e9b0e
Ajit <ajitsr999@gmail.com>
2024-02-09 21:59:59 +0530

    Added 3d custom control button

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,8 +1,9 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import maplibregl, { Map } from 'maplibre-gl';
 import { environment } from 'src/environments/environment';
-import { Basemap } from 'src/models/basemap';
-import { basemapList } from 'src/utilities/mapCommon';
+import { Basemap } from 'src/app/models/basemap';
+import { basemapList } from 'src/app/utilities/mapCommon';
+import { MapService } from '../services/mapService/map.service';
 
 @Component({
   selector: 'app-map',
@@ -18,16 +19,21 @@ export class MapComponent {
   basemapList: Basemap[] = basemapList;
   selectedBasemapIndex = 0
 
+  constructor(private mapService: MapService) { }
+
   ngAfterViewInit(): void {
     this.map = new Map({
       container: this.mapRef.nativeElement, // container id
       style: this.basemapList[this.selectedBasemapIndex].url, // style URL
-      center: [0, 0], // starting position [lng, lat]
-      zoom: 1 // starting zoom
+      center: [-74.0349617, 40.7148305], // starting position [lng, lat]
+      zoom: 15, // starting zoom,
+      // pitch: 60
     })
 
     this.map.addControl(new maplibregl.NavigationControl())
     this.map.addControl(new maplibregl.FullscreenControl())
+
+    this.mapService.addPitchToggleControl(this.map);
 
   }
 
--- src/app/utilities/mapCommon.ts
+++ src/app/utilities/mapCommon.ts
@@ -1,10 +1,14 @@
 import { environment } from "src/environments/environment";
-import { Basemap } from "src/models/basemap";
+import { Basemap } from "src/app/models/basemap";
 
 
 const maptilerKey = environment.maptilerKey;
 
 export const basemapList: Basemap[] = [
+    {
+        url: `https://api.maptiler.com/maps/topo-v2/style.json?key=${maptilerKey}`,
+        image: "assets/maps/topo-v2.png"
+    },
     {
         url: `https://api.maptiler.com/maps/basic-v2/style.json?key=${maptilerKey}`,
         image: "assets/maps/basic-v2.png"
@@ -12,9 +16,5 @@ export const basemapList: Basemap[] = [
     {
         url: `https://api.maptiler.com/maps/satellite/style.json?key=${maptilerKey}`,
         image: "assets/maps/satellite.png"
-    },
-    {
-        url: `https://api.maptiler.com/maps/topo-v2/style.json?key=${maptilerKey}`,
-        image: "assets/maps/topo-v2.png"
     }
 ]
